799a796934ea66a85c602f2436a4a2b31994937a
Adolfo Augusto R <adolfoaugusto2010@hotmail.com>
2021-03-06 10:11:16 -0300

    migrations category product and coupon

--- database/migrations/1615034080361_product_schema.js
+++ database/migrations/1615034080361_product_schema.js
@@ -24,9 +24,20 @@ class ProductSchema extends Schema {
       table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
       table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
     })
+
+    this.create('category_product', (table) => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+      table.foreign('category_id').references('id').inTable('categories').onDelete('cascade')
+
+    })
   }
 
   down () {
+    this.drop('category_product')
     this.drop('image_product')
     this.drop('products')
   }
