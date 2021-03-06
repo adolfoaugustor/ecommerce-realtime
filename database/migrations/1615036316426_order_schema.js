9b296de39cb0b54f9942768e0268f10e4e149955
Adolfo Augusto R <adolfoaugusto2010@hotmail.com>
2021-03-06 10:26:52 -0300

    migration order and relation order by coupon

--- database/migrations/1615036316426_order_schema.js
+++ database/migrations/1615036316426_order_schema.js
@@ -7,8 +7,13 @@ class OrderSchema extends Schema {
   up () {
     this.create('orders', (table) => {
       table.increments()
-      
+      table.decimal('total', 12, 2).defaultTo(0)
+      table.integer('user_id').unsigned()
+      table.enu('status', ['pending', 'cancelled', 'shipped', 'paid', 'finished'])
+
       table.timestamps()
+
+      table.foreign('user_id').references('id').inTable('users').onDelete('cascade')
     })
   }
 
