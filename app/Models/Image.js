383aa6ba42bf4715df73efe4346dbe95ca25a0f6
Adolfo Augusto R <adolfoaugusto2010@hotmail.com>
2021-03-07 17:48:55 -0300

    finish model image

--- app/Models/Image.js
+++ app/Models/Image.js
@@ -2,8 +2,16 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Env = use('Env')
+
+static get computed(){
+    return ['url']
+}
 
 class Image extends Model {
+    getUrl({path}){
+        return `${Env.get('APP_URL')}/images/${path}`
+    }
 }
 
 module.exports = Image
