a185ba0ff4c3479fc84850f82add93171fe67409
Adolfo Augusto R <adolfoaugusto@users.noreply.github.com>
2021-08-14 16:34:48 -0300

    Update ImageController.js

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -6,6 +6,7 @@
 
 const Image = use('App/Models/Image')
 const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers')
+const fs = use('fs')
 
 /**
  * Resourceful controller for interacting with images
@@ -69,9 +70,25 @@ class ImageController {
 
       let files = await manage_multiple_uploads(fileJar)
       
-      await Promise.all()
+      await Promise.all(
+        files.success.map(async file => {
+          const image = await Image.create({
+            path: file.filename,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+          images.push(image)
+        })
+      )
+
+      return response.status(201).send({ success: images, errors: files.error})
+
     } catch (error) {
-      
+      console.error(error) 
+      return response.status(400).send({
+        message: "Não foi possível processar a sua solicitação!"
+      })
     }
   }
 
@@ -84,19 +101,9 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing image.
-   * GET images/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show ({ params: { id }, request, response, view }) {
+    const image = await Image.findOrFail(id)
+    return response.send(image)
   }
 
   /**
@@ -107,7 +114,23 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+    const image = await Image.findOrFail(id)
+
+    try {
+      image.merge(request.only(['original_name']))
+
+      await image.save()
+
+      return response.status(200).send(image)
+
+    } catch (error) {
+      
+      return response.status(400).send({
+        message: 'Não foi possivel atualizar esta imagem no momento!'
+      })
+
+    }
   }
 
   /**
@@ -118,7 +141,26 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const image = await Image.findOrFail(id)
+
+    try {
+      let filepath = Helpers.publicPath(`uploads/${image.path}`)
+
+
+      await fs.unlink(filepath, err => {  
+        if(!err)
+        
+        await image.delete()
+      })
+
+
+      return response.status(201).send()
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possivel error esta imagem no momento!'
+      })
+    }
   }
 }
 
